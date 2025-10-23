b23319f98fc92afc61bd43c20936f4df7fb679c6
BaekSungJun <por9535@gmail.com>
2025-10-23 15:19:03 +0900

    feat:임시 저장

--- src/main/java/store/bookcamp/api/member/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package store.bookcamp.api.member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import store.bookcamp.api.member.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
