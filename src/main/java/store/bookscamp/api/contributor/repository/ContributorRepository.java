b28ae711f129c5b2a40b6f4e25ce0f4ce83d4b0e
김가현 <kimgh@gimgahyeon-ui-MacBookAir.local>
2025-10-30 17:30:58 +0900

    feat: 기여자 페이지어블 임포트 삭제

--- src/main/java/store/bookscamp/api/contributor/repository/ContributorRepository.java
+++ src/main/java/store/bookscamp/api/contributor/repository/ContributorRepository.java
@@ -1,8 +1,6 @@
 package store.bookscamp.api.contributor.repository;
 
 import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import store.bookscamp.api.contributor.entity.Contributor;
 
