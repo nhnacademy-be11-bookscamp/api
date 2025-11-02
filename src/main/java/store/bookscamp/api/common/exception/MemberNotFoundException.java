e0ef7579f9cd5c699649e053eb8e0b24d91a677f
BaekSungJun <por9535@gmail.com>
2025-11-02 19:24:28 +0900

    feat: member 로직 수정 및 email,phone 예외처리 추가

--- src/main/java/store/bookscamp/api/common/exception/MemberNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package store.bookscamp.api.common.exception;
-
-public class MemberNotFoundException extends ApplicationException {
-    public MemberNotFoundException(ErrorCode errorCode) {
-        super(errorCode);
-    }
-}
