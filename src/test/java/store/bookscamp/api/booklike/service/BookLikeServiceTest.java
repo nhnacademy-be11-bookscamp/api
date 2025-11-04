9d9a4eb15ac2948a1e882d8b98739fdf2ae6df74
Yoon-Bin99 <shp06135@naver.com>
2025-11-04 16:23:06 +0900

    의문의 테스트 코드 처단2

--- src/test/java/store/bookscamp/api/booklike/service/BookLikeServiceTest.java
+++ src/test/java/store/bookscamp/api/booklike/service/BookLikeServiceTest.java
@@ -1,5 +1,6 @@
 package store.bookscamp.api.booklike.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
