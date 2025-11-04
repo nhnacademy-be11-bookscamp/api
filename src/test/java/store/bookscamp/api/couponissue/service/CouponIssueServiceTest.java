1fc7aca65050d8e45d347d1ad556c3fda72e79a6
eahc00 <eahc00@naver.com>
2025-11-04 15:17:57 +0900

    test: 회원가입 쿠폰 테스트 구현

---
+++ src/test/java/store/bookscamp/api/couponissue/service/CouponIssueServiceTest.java
@@ -0,0 +1,153 @@
+package store.bookscamp.api.couponissue.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static store.bookscamp.api.common.exception.ErrorCode.COUPON_ISSUE_ALREADY_EXIST;
+import static store.bookscamp.api.common.exception.ErrorCode.COUPON_NOT_FOUND;
+import static store.bookscamp.api.common.exception.ErrorCode.MEMBER_NOT_FOUND;
+import static store.bookscamp.api.coupon.entity.DiscountType.AMOUNT;
+import static store.bookscamp.api.coupon.entity.TargetType.WELCOME;
+import static store.bookscamp.api.member.entity.MemberStatus.NORMAL;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import store.bookscamp.api.common.exception.ApplicationException;
+import store.bookscamp.api.coupon.entity.Coupon;
+import store.bookscamp.api.coupon.repository.CouponRepository;
+import store.bookscamp.api.couponissue.entity.CouponIssue;
+import store.bookscamp.api.couponissue.repository.CouponIssueRepository;
+import store.bookscamp.api.member.entity.Member;
+import store.bookscamp.api.member.repository.MemberRepository;
+
+@SpringBootTest
+@Transactional
+class CouponIssueServiceTest {
+
+    @Autowired
+    private CouponIssueService couponIssueService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private CouponIssueRepository couponIssueRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Nested
+    @DisplayName("issueWelcomeCoupon 통합 테스트")
+    class IssueWelcomeCoupon {
+
+        @Test
+        @DisplayName("정상적으로 웰컴 쿠폰이 발급된다.")
+        void issueWelcomeCoupon_success() {
+            // given
+            Member member = memberRepository.save(new Member(
+                    "회원",
+                    "1234",
+                    "member@naver.com",
+                    "01012345678",
+                    0,
+                    NORMAL,
+                    LocalDate.now(),
+                    "member",
+                    LocalDateTime.now(),
+                    LocalDate.of(2001, 1, 1)
+            ));
+
+             couponRepository.save(new Coupon(
+                    WELCOME,
+                    null,
+                    AMOUNT,
+                    10000,
+                    50000,
+                    10000,
+                    30)
+            );
+
+            // when
+            Long issueId = couponIssueService.issueWelcomeCoupon(member.getId());
+
+            // then
+            List<CouponIssue> issuedCoupons = couponIssueRepository.findAll();
+
+            assertThat(issuedCoupons).hasSize(1);
+            assertThat(issuedCoupons.get(0).getCoupon().getTargetType()).isEqualTo(WELCOME);
+            assertThat(issuedCoupons.get(0).getMember().getId()).isEqualTo(member.getId());
+            assertThat(issueId).isEqualTo(issuedCoupons.get(0).getId());
+        }
+
+        @Test
+        @DisplayName("이미 웰컴 쿠폰을 발급받은 회원이면 예외 발생")
+        void issueWelcomeCoupon_alreadyIssued() {
+            // given
+            Member member = memberRepository.save(new Member(
+                    "회원",
+                    "1234",
+                    "member@naver.com",
+                    "01012345678",
+                    0,
+                    NORMAL,
+                    LocalDate.now(),
+                    "member",
+                    LocalDateTime.now(),
+                    LocalDate.of(2001, 1, 1)
+            ));
+            Coupon coupon = couponRepository.save(new Coupon(
+                    WELCOME,
+                    null,
+                    AMOUNT,
+                    10000,
+                    50000,
+                    10000,
+                    30
+                    ));
+            couponIssueRepository.save(new CouponIssue(coupon, member, LocalDateTime.now().plusDays(coupon.getValidDays())));
+
+            // when & then
+            assertThatThrownBy(() -> couponIssueService.issueWelcomeCoupon(member.getId()))
+                    .isInstanceOf(ApplicationException.class)
+                    .hasMessageContaining(COUPON_ISSUE_ALREADY_EXIST.getMessage());
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 회원이면 예외 발생")
+        void issueWelcomeCoupon_memberNotFound() {
+            // when & then
+            assertThatThrownBy(() -> couponIssueService.issueWelcomeCoupon(999L))
+                    .isInstanceOf(ApplicationException.class)
+                    .hasMessageContaining(MEMBER_NOT_FOUND.getMessage());
+        }
+
+        @Test
+        @DisplayName("웰컴 쿠폰이 존재하지 않으면 예외 발생")
+        void issueWelcomeCoupon_couponNotFound() {
+            // given
+            Member member = memberRepository.save(new Member(
+                    "회원",
+                    "1234",
+                    "member@naver.com",
+                    "01012345678",
+                    0,
+                    NORMAL,
+                    LocalDate.now(),
+                    "member",
+                    LocalDateTime.now(),
+                    LocalDate.of(2001, 1, 1)
+            ));
+
+            // when & then
+            assertThatThrownBy(() -> couponIssueService.issueWelcomeCoupon(member.getId()))
+                    .isInstanceOf(ApplicationException.class)
+                    .hasMessageContaining(COUPON_NOT_FOUND.getMessage());
+        }
+    }
+}
