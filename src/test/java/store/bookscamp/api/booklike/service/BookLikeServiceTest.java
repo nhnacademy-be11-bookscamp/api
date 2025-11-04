aa4393465da69bfef4ee1b40b85b769f21cf9d5f
Yoon-Bin99 <shp06135@naver.com>
2025-11-04 21:30:17 +0900

    [좋아요] 의문의 테스트 코드 처단 (#49)
    * entity pull
    
    * 도서 정렬 기능(제목순, 가격순, 조회순) 구현
    
    * 도서 정렬 기능 (제목순, 가격순, 조회순) 구현
    
    * [Feature/entity] 필드 primitive 타입 적용 및 order 테이블 orderInfo로 수정 (#10)
    
    * feat: 필드 primitive 타입 적용 및 order 테이블 orderInfo로 수정
    
    * feat: FetchType.LAZY 직접 명시하도록 수정
    
    * 카테고리 조회 기능 수정
    
    * 도서 조회 기능
    
    * 피드백 반영 코드 수정
    
    * test2
    
    * 카테고리 계층 구조 구현
    
    * yam 누락 설정 복구
    
    * book detail page
    
    * DB 관련 yml 변경
    
    * 카테고리 관리자 기능 구현(CRUD)
    
    * 기여자 수정 반영
    
    * 좋아요 브랜치
    
    * 좋아요 기능 구현
    
    * 테스트 소매넣기 차단
    
    * 의문의 테스트 코드 처단2
    
    ---------
    
    Co-authored-by: kiju-dev <kijujeong.dev@gmail.com>

--- src/test/java/store/bookscamp/api/booklike/service/BookLikeServiceTest.java
+++ src/test/java/store/bookscamp/api/booklike/service/BookLikeServiceTest.java
@@ -1,5 +1,6 @@
 package store.bookscamp.api.booklike.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.HashOperations;
 
 @SpringBootTest
-class BookLikeServiceIntegrationTest {
+@Disabled
+class BookLikeServiceTest {
 
     @Autowired
     private BookLikeService likeService;
