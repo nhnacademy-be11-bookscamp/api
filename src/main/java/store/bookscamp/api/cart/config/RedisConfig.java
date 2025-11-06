81b94affd96d5d09b1b57f00e41b9ff488ceeb70
Kiju Jeong <kijujeong.dev@gmail.com>
2025-11-06 18:16:27 +0900

    redis config 수정

--- src/main/java/store/bookscamp/api/cart/config/RedisConfig.java
+++ src/main/java/store/bookscamp/api/cart/config/RedisConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -19,7 +20,13 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperty.host(), redisProperty.port());
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisProperty.host());
+        config.setPort(redisProperty.port());
+        config.setPassword(redisProperty.password());  // 비밀번호 설정
+        config.setDatabase(redisProperty.database());
+
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
