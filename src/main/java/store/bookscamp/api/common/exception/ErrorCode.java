18e998e146de82f1b9aa8fd55810ee628b8e494d
Cherrrryk <kimcherry01@naver.com>
2025-11-13 16:19:16 +0900

    Feature/point (#114)
    * feat: 회원가입 포인트, 스웨거 적용
    
    * feat: 회원가입 포인트 발행
    
    * fix: listener 병합
    
    * fix: X-userId 추가

--- src/main/java/store/bookscamp/api/common/exception/ErrorCode.java
+++ src/main/java/store/bookscamp/api/common/exception/ErrorCode.java
@@ -74,6 +74,8 @@ public enum ErrorCode {
     POINT_NOT_ALLOWED_FOR_NON_MEMBER(BAD_REQUEST, "비회원은 포인트를 사용할 수 없습니다."),
 
     CART_ITEM_NOT_COMPLETE(BAD_REQUEST, "일부 장바구니 아이템을 찾을 수 없습니다."),
+
+    ORDER_NOT_FOUND(NOT_FOUND, "주문을 찾을 수 없습니다.")
     ;
 
 
--- src/main/java/store/bookscamp/api/pointpolicy/repository/PointPolicyRepository.java
+++ src/main/java/store/bookscamp/api/pointpolicy/repository/PointPolicyRepository.java
@@ -1,7 +1,14 @@
 package store.bookscamp.api.pointpolicy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import store.bookscamp.api.coupon.entity.Coupon;
+import store.bookscamp.api.coupon.entity.TargetType;
 import store.bookscamp.api.pointpolicy.entity.PointPolicy;
+import store.bookscamp.api.pointpolicy.entity.PointPolicyType;
+
+import java.util.Optional;
 
 public interface PointPolicyRepository extends JpaRepository<PointPolicy, Long> {
+
+    Optional<PointPolicy> findByPointPolicyType(PointPolicyType pointPolicyType);
 }
