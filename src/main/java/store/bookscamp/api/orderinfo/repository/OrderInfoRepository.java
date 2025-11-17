ba2c7d87ccc6c78910e30665917000b1ed278493
김가현 <kimgh@gimgahyeon-ui-MacBookAir.local>
2025-11-17 18:15:13 +0900

    feat:feature/orderList 작성 중

--- src/main/java/store/bookscamp/api/orderinfo/repository/OrderInfoRepository.java
+++ src/main/java/store/bookscamp/api/orderinfo/repository/OrderInfoRepository.java
@@ -1,7 +1,19 @@
 package store.bookscamp.api.orderinfo.repository;
 
+import java.util.Collection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import store.bookscamp.api.orderinfo.entity.OrderInfo;
+import store.bookscamp.api.orderinfo.entity.OrderStatus;
 
 public interface OrderInfoRepository extends JpaRepository<OrderInfo, Long> {
+
+    // 회원 주문 목록 조회 (페이징 + 정렬은 Pageable에서 처리)
+    Page<OrderInfo> findByMemberId(Long orderId, Pageable pageable);
+
+    // TODO : 아직 사용 안함 => 필요 없으면 삭제
+    Page<OrderInfo> findByMemberIdAndOrderStatusIn(Long orderId,
+                                                   Collection<OrderStatus> orderStatus,
+                                                   Pageable pageable);
 }
--- src/main/java/store/bookscamp/api/orderitem/repository/OrderItemRepository.java
+++ src/main/java/store/bookscamp/api/orderitem/repository/OrderItemRepository.java
@@ -1,7 +1,11 @@
 package store.bookscamp.api.orderitem.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import store.bookscamp.api.orderitem.entity.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    // TODO : 주문 내역 상세 조회할 때 필요할지도?
+    List<OrderItem> findByOrderId(Long orderId);
 }
