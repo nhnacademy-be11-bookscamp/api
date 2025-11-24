96997595754930b45488e002fa3054c2c1d089e2
eahc00 <eahc00@naver.com>
2025-11-24 13:58:13 +0900

    test: 테스트 코드 추가

--- src/main/java/store/bookscamp/api/couponissue/entity/CouponIssue.java
+++ src/main/java/store/bookscamp/api/couponissue/entity/CouponIssue.java
@@ -45,17 +45,6 @@ public class CouponIssue extends SoftDeleteEntity {
 
     public CouponIssue(Coupon coupon,
                        Member member,
-                       LocalDateTime expiredAt,
-                       LocalDateTime usedAt
-    ) {
-        this.coupon = coupon;
-        this.member = member;
-        this.expiredAt = expiredAt;
-        this.usedAt = usedAt;
-    }
-
-    public CouponIssue(Coupon coupon,
-                       Member member,
                        LocalDateTime expiredAt
     ) {
         this.coupon = coupon;
