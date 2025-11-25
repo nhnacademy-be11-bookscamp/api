14c239c145146a73be6b244fbee5f36a81c9bc64
김가현 <kimgh@gimgahyeon-ui-MacBookAir.local>
2025-11-25 14:02:52 +0900

    feat: OrderInfo TestCode 작성

--- src/main/java/store/bookscamp/api/member/entity/Member.java
+++ src/main/java/store/bookscamp/api/member/entity/Member.java
@@ -14,6 +14,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
@@ -25,6 +27,8 @@ import store.bookscamp.api.rank.entity.Rank;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
 @SQLDelete(sql = "UPDATE `member` SET deleted_at = NOW(), status = 'WITHDRAWN' WHERE id = ?")
 @SQLRestriction("deleted_at IS NULL")
