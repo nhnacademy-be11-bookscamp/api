76bf594ea8fe21da1dde3ebbfa0b77cd7584ae28
Yoon-Bin99 <shp06135@naver.com>
2025-11-28 12:42:50 +0900

    랭크 업데이트 실패 방어 로직 구현

--- src/main/java/store/bookscamp/api/rank/service/RankService.java
+++ src/main/java/store/bookscamp/api/rank/service/RankService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import store.bookscamp.api.common.exception.ApplicationException;
@@ -44,6 +46,7 @@ public class RankService {
         return new RankGetDto(name, value);
     }
 
+    @Retryable(noRetryFor = ApplicationException.class, backoff = @Backoff(multiplier = 2.0, maxDelay = 10000), listeners = "customRetryListener")
     @Transactional
     public void updateAllMemberGrades() {
 
