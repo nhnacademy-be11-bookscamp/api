4d93ce09f3b993c06658beed482ae3c20fbab40d
김가현 <kimgh@gimgahyeon-ui-MacBookAir.local>
2025-12-02 21:42:30 +0900

    feat: NonMemberOrderControllerTest 작성 완료

--- src/main/java/store/bookscamp/api/common/exception/ErrorCode.java
+++ src/main/java/store/bookscamp/api/common/exception/ErrorCode.java
@@ -93,9 +93,6 @@ public enum ErrorCode {
     ORDER_ALREADY_PAID(BAD_REQUEST, "이미 결제된 주문입니다."),
     ORDER_NOT_AWAITING_PAYMENT(BAD_REQUEST, "결제 대기 중인 주문이 아닙니다."),
     ORDER_CANNOT_BE_CANCELLED(BAD_REQUEST, "취소할 수 없는 주문입니다."),
-<<<<<<< HEAD
-    ORDER_PASSWORD_INVALID(NOT_FOUND, "주문이 존재하지 않습니다.")
-=======
     ORDER_PASSWORD_INVALID(NOT_FOUND, "주문이 존재하지 않습니다."),
 
     ORDER_STATUS_ALREADY_SET(BAD_REQUEST, "이미 해당 상태로 설정되어 있습니다."),
@@ -104,9 +101,7 @@ public enum ErrorCode {
     DELIVERED_ORDER_CAN_ONLY_BE_RETURNED(BAD_REQUEST, "배송 완료된 주문은 반품만 가능합니다."),
     AWAITING_PAYMENT_ORDER_CANNOT_BE_UPDATED(BAD_REQUEST, "결제 대기 중인 주문은 관리자가 변경할 수 없습니다."),
     INVALID_ORDER_STATUS_TRANSITION(BAD_REQUEST, "유효하지 않은 주문 상태 전환입니다.")
->>>>>>> ddba3c09e4bbf95bade7403bc78214ae00f98e11
     ;
-
 
     private HttpStatus httpStatus;
     private String message;
--- src/main/java/store/bookscamp/api/orderinfo/service/OrderDetailService.java
+++ src/main/java/store/bookscamp/api/orderinfo/service/OrderDetailService.java
@@ -45,8 +45,6 @@ public class OrderDetailService {
         return toOrderDetailResponse(orderInfo, orderItems);
     }
 
-<<<<<<< HEAD
-=======
     public OrderDetailResponse getOrderDetailForAdmin(Long orderId) {
         OrderInfo orderInfo = orderInfoRepository.findById(orderId)
                 .orElseThrow(() -> new ApplicationException(ErrorCode.ORDER_NOT_FOUND));
@@ -56,57 +54,6 @@ public class OrderDetailService {
         return toOrderDetailResponse(orderInfo, orderItems);
     }
 
->>>>>>> ddba3c09e4bbf95bade7403bc78214ae00f98e11
-
-
-//        List<OrderDetailItemResponse> itemResponses = orderItems.stream()
-//                .map(item -> new OrderDetailItemResponse(
-//                        item.getBook().getId(),
-//                        item.getBook().getTitle(),
-//                        item.getOrderQuantity(),
-//                        item.getBookPrice(),
-//                        item.getBookTotalAmount()
-//                ))
-//                .collect(Collectors.toList());
-//
-//        String recipientName = null;
-//        String recipientPhone = null;
-//        String deliveryAddress = null;
-//        String deliveryMemo = null;
-//
-//        if(orderInfo.getDelivery() != null) {
-//            var delivery = orderInfo.getDelivery();
-//            recipientName = delivery.getRecipientName();
-//            recipientPhone = delivery.getRecipientPhone();
-//            deliveryAddress = delivery.getRoadNameAddress() + " " + delivery.getDetailAddress();
-//            deliveryMemo = delivery.getDeliveryMemo();
-//
-//        }
-//        int productAmount = orderInfo.
-//                getNetAmount();
-//        int deliveryFee = orderInfo.getDeliveryFee();
-//        int packagingFee = orderInfo.getPackagingFee();
-//        int discountAmount = orderInfo.getDiscountAmount();
-//        int usedPoint = orderInfo.getUsedPoint();
-//        int finalPaymentAmount = orderInfo.getFinalPaymentAmount();
-//
-//        return new OrderDetailResponse(
-//                orderInfo.getId(),
-//                orderInfo.getCreatedAt(),
-//                orderInfo.getOrderStatus().name(),
-//                itemResponses,
-//                recipientName,
-//                recipientPhone,
-//                deliveryAddress,
-//                deliveryMemo,
-//                productAmount,
-//                deliveryFee,
-//                packagingFee,
-//                discountAmount,
-//                usedPoint,
-//                finalPaymentAmount
-//        );
-
 
     // 비회원부분
     public OrderDetailResponse getNonMemberOrderDetail(String orderNumber, NonMemberInfoDto nonMemberInfoDto) {
@@ -114,13 +61,6 @@ public class OrderDetailService {
         NonMember nonMember = nonMemberRepository.findByOrderInfo_OrderNumber(orderNumber)
                 .orElseThrow(() -> new ApplicationException(ErrorCode.ORDER_NOT_FOUND));
 
-        // TODO : 비밀번호 검증 어떻게 하는건데....???
-//        String rawPassword = nonMemberInfoDto.password();
-//        String encodedPassword = nonMember.getPassword();
-//
-//        if( !passwordEncoder.matches(rawPassword, encodedPassword) ) {
-//            throw new ApplicationException(ErrorCode.ORDER_PASSWORD_INVALID);
-//        }
 
         OrderInfo orderInfo = nonMember.getOrderInfo();
         List<OrderItem> orderItems = orderItemRepository.findByOrderInfoId(orderInfo.getId());
